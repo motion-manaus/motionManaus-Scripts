46cb201df67f53ae76816c9f1240ad17f99d4116
motion-manaus <112268029+motion-manaus@users.noreply.github.com>
2022-09-15 01:01:25 -0300

    Create Offset Value + toWorld.jsx

---
+++ Offset Value + toWorld.jsx
@@ -0,0 +1,4 @@
+L = thisComp.layer(index-1); // variavel pra puxar o position da layer acima
+r = L.sourceRectAtTime(time,true) // faço a minima ideia, mas puxa a posição "correta" da layer
+t = thisComp.layer("data-stream-B.Null1").effect("Slider Control")("Slider"); // slider pra controlar offset do tempo
+L.toWorld([r.left + r.width/2,r.top + r.height/2],time-t); // toWorld posiciona a layer da forma correta, não faça perguntas, só aceita
