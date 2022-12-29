d35ddd654191b2e9551fa8ede76c5dd99eb34b4a
motion-manaus <112268029+motion-manaus@users.noreply.github.com>
2022-12-29 17:03:01 -0300

    Update Easy Matte.jsx

--- Easy Matte.jsx
+++ Easy Matte.jsx
@@ -1,4 +1,4 @@
-//Select the layers to be parented. The first selected layer will be the parent.
+//Select the layers to paste the selected matte. The first selected layer will have the Track Matte copied.
  app.beginUndoGroup("MatteCopy");
  if (app.project.activeItem.selectedLayers.length > 1 && app.project.activeItem.selectedLayers[0].hasTrackMatte){ 
  var matteCopy = app.project.activeItem.selectedLayers[0];
