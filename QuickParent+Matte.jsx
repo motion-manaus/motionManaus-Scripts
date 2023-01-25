df53d4047d6dc0b4d7ab05ae7b95af82679cfafc
motion-manaus <112268029+motion-manaus@users.noreply.github.com>
2023-01-25 16:37:10 -0300

    Create QuickParent+Matte.jsx

---
+++ QuickParent+Matte.jsx
@@ -0,0 +1,19 @@
+//Select the layers to be parented. The first selected layer will be the parent.
+ app.beginUndoGroup("Parent Selected Layers");
+ 
+ if (app.project.activeItem.selectedLayers.length > 1){ 
+ var newParent = app.project.activeItem.selectedLayers[0];
+
+ for (var i = 1; i < app.project.activeItem.selectedLayers.length; i++){
+ app.project.activeItem.selectedLayers[i].parent = newParent;
+ app.project.activeItem.selectedLayers[i].transform.position.setValue(parent.transform.anchorPoint.value);
+ app.project.activeItem.selectedLayers[i].setTrackMatte(newParent, TrackMatteType.ALPHA);
+ 
+ 
+   
+
+}
+} else {
+alert("Select the layers to be parented. The first selected layer will be the parent.");
+}
+app.endUndoGroup();
