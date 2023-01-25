e265aee2d248169461cedfc51dfd62118385e92d
motion-manaus <112268029+motion-manaus@users.noreply.github.com>
2023-01-25 15:51:39 -0300

    Update Quick Parent.jsx

--- Quick Parent.jsx
+++ Quick Parent.jsx
@@ -1,11 +1,12 @@
 //Select the layers to be parented. The first selected layer will be the parent.
  app.beginUndoGroup("Parent Selected Layers");
+ 
  if (app.project.activeItem.selectedLayers.length > 1){ 
  var parent = app.project.activeItem.selectedLayers[0];
- var newPosition = [0,0,0];
+
  for (var i = 1; i < app.project.activeItem.selectedLayers.length; i++){
  app.project.activeItem.selectedLayers[i].parent = parent;
- app.project.activeItem.selectedLayers[i].transform.position.setValue(newPosition);
+ app.project.activeItem.selectedLayers[i].transform.position.setValue(parent.transform.anchorPoint.value);
  
    
 
