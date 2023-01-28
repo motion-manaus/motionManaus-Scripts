7bac63004f5e65d16477e1af286b786c3564a7af
motion-manaus <112268029+motion-manaus@users.noreply.github.com>
2023-01-28 10:03:23 -0300

    Update QuickParent+Matte.jsx

--- QuickParent+Matte.jsx
+++ QuickParent+Matte.jsx
@@ -1,19 +1,17 @@
-//Select the layers to be parented. The first selected layer will be the parent.
- app.beginUndoGroup("Parent Selected Layers");
- 
- if (app.project.activeItem.selectedLayers.length > 1){ 
- var newParent = app.project.activeItem.selectedLayers[0];
+// Select the layers to be parented. The first selected layer will be the parent.
+app.beginUndoGroup("Parent Selected Layers");
+var selectedLayers = app.project.activeItem.selectedLayers, newParent, i;
 
- for (var i = 1; i < app.project.activeItem.selectedLayers.length; i++){
- app.project.activeItem.selectedLayers[i].parent = newParent;
- app.project.activeItem.selectedLayers[i].transform.position.setValue(parent.transform.anchorPoint.value);
- app.project.activeItem.selectedLayers[i].setTrackMatte(newParent, TrackMatteType.ALPHA);
- 
- 
-   
-
-}
+if (selectedLayers.length > 1) {
+    newParent = selectedLayers[0];
+    for (i = 1; i < selectedLayers.length; i++) {
+        selectedLayers[i].parent = newParent;
+        selectedLayers[i].transform.position.setValue(newParent.transform.anchorPoint.value);
+        selectedLayers[i].setTrackMatte(newParent, TrackMatteType.ALPHA);
+    }
+} else if (selectedLayers.length === 1) {
+    alert("Select at least one more layer to be parented.");
 } else {
-alert("Select the layers to be parented. The first selected layer will be the parent.");
+    alert("Select the layers to be parented. The first selected layer will be the parent.");
 }
 app.endUndoGroup();
