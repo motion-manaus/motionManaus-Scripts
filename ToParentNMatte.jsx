dc254c93c30b2f32281e63f10fba6d9549dfe5d1
motion-manaus <112268029+motion-manaus@users.noreply.github.com>
2023-02-02 18:52:47 -0300

    Update and rename QuickParent+Matte.jsx to ToParentNMatte.jsx

--- ToParentNMatte.jsx
+++ ToParentNMatte.jsx
@@ -6,8 +6,17 @@ if (selectedLayers.length > 1) {
     newParent = selectedLayers[0];
     for (i = 1; i < selectedLayers.length; i++) {
         selectedLayers[i].parent = newParent;
-        selectedLayers[i].transform.position.setValue(newParent.transform.anchorPoint.value);
-        selectedLayers[i].setTrackMatte(newParent, TrackMatteType.ALPHA);
+		selectedLayers[i].setTrackMatte(newParent, TrackMatteType.ALPHA);
+		  if (selectedLayers[i].transform.position.dimensionsSeparated == true){
+			  selectedLayers[i].transform.xPosition.setValue(newParent.transform.anchorPoint.value[0]);
+			  selectedLayers[i].transform.yPosition.setValue(newParent.transform.anchorPoint.value[1]);
+				if (selectedLayers[i].threeDLayer === true) {
+				selectedLayers[i].transform.zPosition.setValue(newParent.transform.anchorPoint.value[2]);
+				}
+			}else{
+				selectedLayers[i].transform.position.setValue(newParent.transform.anchorPoint.value);
+			}
+		
     }
 } else if (selectedLayers.length === 1) {
     alert("Select at least one more layer to be parented.");
