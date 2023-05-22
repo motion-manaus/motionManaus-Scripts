136c35e047c4084b16b6689057e18fd2e6fddc19
motion-manaus <112268029+motion-manaus@users.noreply.github.com>
2023-05-22 16:29:11 -0300

    Create JustMatte.jsx

---
+++ JustMatte.jsx
@@ -0,0 +1,14 @@
+// Select the layers to be parented. The first selected layer will be the parent.
+app.beginUndoGroup("Matte Selected Layers");
+var selectedLayers = app.project.activeItem.selectedLayers, newParent, i;
+
+if (selectedLayers.length > 1) {
+    newParent = selectedLayers[0];
+    for (i = 1; i < selectedLayers.length; i++) {
+		selectedLayers[i].setTrackMatte(newParent, TrackMatteType.ALPHA);
+} else if (selectedLayers.length === 1) {
+    alert("Select at least one more layer to create a matte.");
+} else {
+    alert("Select the layers to create a matte. The first selected layer will be the matte.");
+}
+app.endUndoGroup();
