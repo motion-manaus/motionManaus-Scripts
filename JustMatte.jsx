0cd06983e41013212417e140e349b91a77293dfc
motion-manaus <112268029+motion-manaus@users.noreply.github.com>
2023-05-22 16:37:38 -0300

    Update JustMatte.jsx

--- JustMatte.jsx
+++ JustMatte.jsx
@@ -3,12 +3,11 @@ app.beginUndoGroup("Matte Selected Layers");
 var selectedLayers = app.project.activeItem.selectedLayers, newParent, i;
 
 if (selectedLayers.length > 1) {
-    newParent = selectedLayers[0];
-    for (i = 1; i < selectedLayers.length; i++) {
+	newParent = selectedLayers[0];
+	for (i = 1; i < selectedLayers.length; i++) {
 		selectedLayers[i].setTrackMatte(newParent, TrackMatteType.ALPHA);
-} else if (selectedLayers.length === 1) {
-    alert("Select at least one more layer to create a matte.");
-} else {
-    alert("Select the layers to create a matte. The first selected layer will be the matte.");
-}
+	}
+	} else {
+		alert("Select the layers to create a matte. The first selected layer will be the matte.");
+	}
 app.endUndoGroup();
