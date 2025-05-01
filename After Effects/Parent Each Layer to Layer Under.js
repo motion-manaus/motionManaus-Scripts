81719bd1b5f3e2a2474fcc72218ccfade2be1934
Manaus <112268029+motion-manaus@users.noreply.github.com>
2025-04-30 23:04:57 -0300

    Create Parent Each Layer to Layer Under.js

---
+++ After Effects/Parent Each Layer to Layer Under.js
@@ -0,0 +1,23 @@
+/**
+ * Parents each selected layer to the layer under it.
+ *
+ * @author Zack Lovatt <zack@lova.tt>
+ * @version 0.2.1
+ */
+(function parentEachToAbove() {
+  var comp = app.project.activeItem;
+
+  if (!(comp && comp instanceof CompItem)) {
+    alert('Please select a composition!');
+    return;
+  }
+
+  app.beginUndoGroup('Parent Selected to Above');
+
+  for (var ii = 0; ii < comp.selectedLayers.length; ii++) {
+    var layer = comp.selectedLayers[ii];
+    layer.parent = comp.layer(layer.index + 1);
+  }
+
+  app.endUndoGroup();
+})();
